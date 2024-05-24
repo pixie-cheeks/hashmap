b656e34fa451ec4dcd7cd9cf195a9f8158e5a47d
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-05-24 23:22:05 +0530

    Pass all tests

--- src/test-hashmap.js
+++ src/test-hashmap.js
@@ -4,6 +4,7 @@ const hashMap = new HashMap();
 
 hashMap.set('ben', 10);
 hashMap.set('kevin', 11);
+hashMap.clear();
 hashMap.set('gwen', 10);
 hashMap.set('Alpha', 'male');
 hashMap.set('Key', 'value');
@@ -19,8 +20,13 @@ hashMap.set('13', 13);
 hashMap.set('14', 14);
 hashMap.set('15', 15);
 
-console.log(hashMap.get('ben'));
-console.log(hashMap.get('bena'));
+console.log(hashMap.get('Bruno'));
+console.log(hashMap.get('Bruna'));
 console.log(hashMap.has('Illegal'));
+console.log(hashMap.has('Illegaal'));
 console.log(hashMap.remove('Alpha'));
+console.log(hashMap.remove('tena'));
+console.log(hashMap.length());
+console.log(hashMap.keys());
+console.log(hashMap.values());
 console.log(hashMap.entries());
